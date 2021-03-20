519b4b13353a6091077118d006a35887626bf9cc
sunghoan-we <javarajava@hotmail.com>
2021-03-19 17:17:53 -0700

    modify

--- app/src/main/java/com/example/moneyjars/ReportActivity.java
+++ app/src/main/java/com/example/moneyjars/ReportActivity.java
@@ -1,14 +1,41 @@
 package com.example.moneyjars;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.utils.Easing;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportActivity extends AppCompatActivity {
+    private LineChart chart;
+    PieChart pieChart;
+    PieData pieData;
+    List<PieEntry> pieEntryList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        pieChart = findViewById(R.id.pieChart);
+        pieChart.setUsePercentValues(true);
+        pieEntryList.add(new PieEntry(10,"India"));
+        pieEntryList.add(new PieEntry(5,"US"));
+        pieEntryList.add(new PieEntry(7,"UK"));
+        pieEntryList.add(new PieEntry(3,"NZ"));
+        PieDataSet pieDataSet = new PieDataSet(pieEntryList,"country");
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 }
