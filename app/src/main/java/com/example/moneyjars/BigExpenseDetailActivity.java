bd366263a991235fd01050c85e4cd020a70c2cf5
sunghoan-we <javarajava@hotmail.com>
2021-03-29 20:00:50 -0700

    modify UserInformation

--- app/src/main/java/com/example/moneyjars/BigExpenseDetailActivity.java
+++ app/src/main/java/com/example/moneyjars/BigExpenseDetailActivity.java
@@ -10,5 +10,6 @@ public class BigExpenseDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_expense_detail);
+        aa
     }
 }
